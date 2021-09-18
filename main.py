a1952cd8343e77c6a2892b9598126200e6d20931
pranavkp1661 <pranavp7645357@gmail.com>
2021-09-18 13:50:39 +0530

    code finish

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import prob_calculator
+from unittest import main
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+hat = prob_calculator.Hat(blue=4, red=2, green=6)
+probability = prob_calculator.experiment(
+    hat=hat,
+    expected_balls={"blue": 2,
+                    "red": 1},
+    num_balls_drawn=4,
+    num_experiments=3000)
+print("Probability:", probability)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Run unit tests automatically
+main(module='test_module', exit=False)
